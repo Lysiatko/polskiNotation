b1053528d44b9a275bc01343b20ae319ed296ddd
MoosieySroosiey <kl1timch@gmail.com>
2023-12-16 04:02:57 +0100

    2.0
    wowzers

--- polskiNotation.py
+++ polskiNotation.py
@@ -1,52 +1,70 @@
 import tkinter as tk
-from tkinter import scrolledtext
+from tkinter import scrolledtext, filedialog, messagebox
 import spacy
+import threading
 
 nlp = spacy.load("pl_core_news_sm")
 
 def parse_text():
-    input_text = text_input.get('1.0', tk.END)
-    doc = nlp(input_text)
-    output_text = ""
+    try:
+        input_text = text_input.get('1.0', tk.END)
+        doc = nlp(input_text)
+        output_text = ""
 
-    for token in doc:
-        if token.pos_ == "NOUN":
-            prefix = "r"
-        elif token.pos_ == "VERB":
-            prefix = "c"
-        elif token.pos_ == "ADJ":
-            prefix = "przym"
-        elif token.pos_ == "PRON":
-            prefix = "z"
-        elif token.pos_ == "ADV":
-            prefix = "przys"
-        elif token.pos_ == "ADP":
-            prefix = "przyi"
-        elif token.pos_ == "CONJ":
-            prefix = "spój"
-        elif token.pos_ == "INTJ":
-            prefix = "wykrz"
-        elif token.pos_ == "NUM":
-            prefix = "licz"
-        elif token.pos_ == "PART":
-            prefix = "par"
-        else:
-           prefix = "i"
-        output_text += prefix + token.text + " "
+        for token in doc:
+            prefix_dict = {
+                "NOUN": "r", "VERB": "c", "ADJ": "przym", 
+                "PRON": "z", "ADV": "przys", "ADP": "przyi", 
+                "CONJ": "s", "INTJ": "w", "NUM": "l", 
+                "PART": "par"
+            }
+            prefix = prefix_dict.get(token.pos_, "i")
+            output_text += prefix + token.text + " "
 
-    text_output.delete('1.0', tk.END)
-    text_output.insert('1.0', output_text)
+        text_output.delete('1.0', tk.END)
+        text_output.insert('1.0', output_text)
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
+def on_text_change(event=None):
+    threading.Thread(target=parse_text, daemon=True).start()
+
+def load_text():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            text_input.delete('1.0', tk.END)
+            text_input.insert('1.0', file.read())
+
+def save_text():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt")
+    if file_path:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(text_output.get('1.0', tk.END))
 
 root = tk.Tk()
-root.title("Językonaprawiaczydło")
+root.title("językonaprawiaczydło 2.0")
 
-text_input = scrolledtext.ScrolledText(root, height=10, width=50)
-text_input.pack()
+frame_input = tk.Frame(root)
+frame_input.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+frame_output = tk.Frame(root)
+frame_output.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
-convert_button = tk.Button(root, text="Napraw", command=parse_text)
-convert_button.pack()
+text_input = scrolledtext.ScrolledText(frame_input, height=10, width=50)
+text_input.pack(expand=True, fill='both')
+text_input.bind("<KeyRelease>", on_text_change)
 
-text_output = scrolledtext.ScrolledText(root, height=10, width=50)
-text_output.pack()
+text_output = scrolledtext.ScrolledText(frame_output, height=10, width=50)
+text_output.pack(expand=True, fill='both')
 
+# Menu for extra features
+menu_bar = tk.Menu(root)
+root.config(menu=menu_bar)
+
+file_menu = tk.Menu(menu_bar, tearoff=0)
+file_menu.add_command(label="Load Text", command=load_text)
+file_menu.add_command(label="Save Output", command=save_text)
+menu_bar.add_cascade(label="File", menu=file_menu)
+
+# Start the GUI loop
 root.mainloop()
